a1b41e72fd2f48ad893adfcd863b4ce0b04d64ec
AgustinGHS <gabriel38106@gmail.com>
2023-05-23 13:28:58 -0300

    portfolio

---
+++ codigo.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".multiple-text", {
+	strings: ["Frontend developer", "Backend developer", "Freelancer"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 100,
+	loop: true
+})
