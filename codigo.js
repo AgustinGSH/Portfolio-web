0c20d9cc5c4da7c3bd2ce739f9aefddf921e659b
AgustinGHS <gabriel38106@gmail.com>
2023-08-11 14:46:54 -0300

    Portfolio web

--- codigo.js
+++ codigo.js
@@ -1,5 +1,5 @@
 var typed = new Typed (".multiple-text", {
-	strings: ["Frontend developer", "Backend developer", "Freelancer"],
+	strings: ["Frontend developer", "Backend developer", "Full-stack developer"],
 	typeSpeed: 100,
 	backSpeed: 100,
 	backDelay: 100,
